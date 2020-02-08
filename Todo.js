303ce942215302d93db4207c5d147ef779da312e
Rakib Khan <rakibkhan@live.co.uk>
2020-02-08 17:13:00 +0000

    ...

--- Todo.js
+++ Todo.js
@@ -12,11 +12,15 @@ class App extends React.Component {
         return (
             <div>
                 <h1>Tasks</h1>
-                <h2>There {this.state.numberOfTasks === 1 ? "is" : "are"} currently {this.state.numberOfTasks} Task{this.state.numberOfTasks === 1 ? "" : "s"}</h2>
+                {/* <h2>There {this.state.numberOfTasks === 1 ? "is" : "are"} currently {this.state.numberOfTasks} Task{this.state.numberOfTasks === 1 ? "" : "s"}</h2> */}
+                <h2>There {this.state.tasks.length === 1 ? "is" : "are"} {this.state.tasks.length} {this.state.tasks.length === 1 ? "task" : "tasks"}</h2>
                 <ul>
-                    {this.state.tasks.map((task, i) => (
-                        <li key={i}>{task}</li>
-                    ))}
+                    {this.state.tasks.map((task, i) =>
+                        <li key={i}>
+                            {task}
+                            <button data-index={i} onClick={this.deleteTask}>Delete</button>
+                        </li>
+                    )}
                 </ul>
                 <input onKeyPress={this.handleKeyPress} onChange={this.updateInput} value={this.state.input} />
                 <button onClick={this.addTask}>Add Task</button>
@@ -38,14 +42,16 @@ class App extends React.Component {
 
     addTask = () => {
         if (this.state.input !== '') {
-
             this.setState(state => ({
                 tasks: [...state.tasks, state.input],
                 input: '',
                 numberOfTasks: state.numberOfTasks + 1
             }));
-
         }
+    }
+
+    deleteTask = () => {
+
     }
 }
 
